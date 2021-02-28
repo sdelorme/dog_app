62a9f7e5654749f4ec7b29c16ca9631891c1a4d3
Seth Delorme <delorme.s.m@gmail.com>
2021-02-28 09:57:33 -0600

    adds create action and POST route for new dog

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -3,4 +3,15 @@ class Api::DogsController < ApplicationController
     @index = Dog.all
     render 'index.json.jb'
   end
+  def create
+    @dog = Dog.new(
+    breed: params[:breed],
+    name: params[:name],
+    age: params[:age],
+    color: params[:color],
+    sex: params [:sex]  
+    )
+    @dog.save
+    render 'index.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/dogs" => "dogs#index"
+    post "/dogs" => "dogs#create"
   end
 end
